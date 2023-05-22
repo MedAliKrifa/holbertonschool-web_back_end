e36e7d76940dd5849ec1de2b811aafaf9c2d29a0
MedAliKrifa <daliolimona@gmail.com>
2023-05-22 16:13:33 +0100

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env python3
+import math
+"""
+add that takes a float a and a float
+b as arguments and returns their sum as a float.
+"""
+
+def floor(n: float) -> int:
+    """
+    float
+    """
+    return math.floor(n)
