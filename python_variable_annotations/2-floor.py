3c7818d2aed1a869355cc24748c018e2dc1f9b92
MedAliKrifa <daliolimona@gmail.com>
2023-05-22 16:16:41 +0100

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -8,6 +8,6 @@ which takes a float n as argument and returns the floor of the float.
 
 def floor(n: float) -> int:
     """
-    float
+    returns the floor of the float
     """
     return math.floor(n)
