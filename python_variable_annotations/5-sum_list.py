a6083d8c6eefdf9bd73029f9f98fbb89913481a6
MedAliKrifa <daliolimona@gmail.com>
2023-05-22 16:37:08 +0100

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+Define and annotate variables
+with the specified values
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    sum list
+    """
+    return sum(input_list)
