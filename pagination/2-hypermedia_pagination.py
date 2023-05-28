c0444180ef18f49b2e56afa357eb66ff9307e89f
MedAliKrifa <daliolimona@gmail.com>
2023-05-28 21:15:38 +0100

    2-hypermedia_pagination.py

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@ hypermedia_pagination
 """
 import csv
 import math
-from typing import List, Tuple
+from typing import Tuple, List, Dict, Union
 
 
 class Server:
@@ -26,53 +26,56 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
         """
-        Return a tuple of size two containing a start index and an end index.
+        return a tuple of size two containing a
+        start index and an end index corresponding to the
+        range of indexes to return in a list
+        for those particular pagination parameters.
         """
-        end = page * page_size
-        start = (page - 1) * page_size
+        end: int = page * page_size
+        start: int = 0
+        for i in range(page - 1):
+            start += page_size
         return (start, end)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        Get the dataset page for the given page number and page size.
+        get page Simple pagination
         """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
         dataset = self.dataset()
         start, end = self.index_range(page, page_size)
         if end > len(dataset):
             return []
         return [list(dataset[row]) for row in range(start, end)]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+    def get_hyper(self, page: int = 1, page_size: int = 10) ->\
+            Dict[str, Union[List[List], None, int]]:
         """
-        Get pagination information for the given page number and page size.
+        get page and return dict
         """
-        dataset = self.dataset()
-        total_pages = math.ceil(len(dataset) / page_size)
-        current_page = page
-        next_page = current_page + 1 if current_page < total_pages else None
-        prev_page = current_page - 1 if current_page > 1 else None
+        data: List = self.get_page(page, page_size)
+        size_dataset: int = len(self.dataset())
+        totalPage = math.ceil(size_dataset / page_size)
+        if page - 1 == 0:
+            prev_page = None
+        else:
+            prev_page = page - 1
 
-        page_data = self.get_page(page, page_size)
+        if page + 1 > size_dataset or data == []:
+            next_page = None
+        else:
+            next_page = page + 1
 
-        return {
-            "page_size": len(page_data),
-            "page": current_page,
-            "data": page_data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
-        }
+        if data is not []:
+            pageSize = page_size
 
-
-# Usage example
-server = Server()
-
-# Get page 2 with page size 5
-page_data = server.get_page(2, 5)
-print(page_data)
-
-# Get pagination information for page 2 with page size 5
-hyper_info = server.get_hyper(2, 5)
-print(hyper_info)
+        hateoas: Dict = {'page_size': pageSize,
+                         'page': page,
+                         'data': data,
+                         'next_page': next_page,
+                         'prev_page': prev_page,
+                         'total_pages': totalPage}
+        return hateoas
