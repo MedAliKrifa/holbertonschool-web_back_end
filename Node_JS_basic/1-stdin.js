7b7c60cb7dc880951e2f4f071e98525857b388c9
MedAliKrifa <daliolimona@gmail.com>
2024-01-25 20:35:43 +0100

    node

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,28 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-
-function displayMessage(message) {
-  console.log(message);
-}
-
-function closeProgram() {
-  displayMessage("This important software is now closing");
-  rl.close();
-}
-
-function getName() {
-  displayMessage("Welcome to Holberton School, what is your name?");
-  rl.on('line', (input) => {
-    if (input.trim().toLowerCase() === 'exit') {
-      closeProgram();
-    } else {
-      displayMessage(`Your name is: ${input}`);
-    }
-  });
-}
-
-getName();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
